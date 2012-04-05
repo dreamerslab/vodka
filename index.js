0b3378e36842d8c5b36b7da2f57674fe1b364f80
ben <ben@dreamerslab.com>
2012-04-05 23:44:40 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function vodka( base_dir ){
+  // expose configs to global
+  require( './lib/configs' )( base_dir );
+
+  // expose utils to global
+  require( './lib/utils' );
+
+  // build actions
+  var actions = require( './lib/actions' );
+
+  // build router
+  var map = require( './lib/route' )( actions );
+
+  // dispatch routes
+  require( base_dir + 'routes' )( map );
+};
+
+vodka.version = JSON.parse( fs.readFileSync( __dirname + '/package.json', 'utf8' )).version;
+
+/**
+ * Exports module.
+ */
+module.exports = vodka;
