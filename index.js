f224eaef6fc78e72d0a389bdadcfed89ee8ddc36
ben <ben@dreamerslab.com>
2013-12-28 00:43:32 -0800

    update vokda for mocha intergration

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
-var fs = require( 'fs' );
+var fs    = require( 'fs' );
+var vodka = require( './lib/vodka' );
 
-function vodka( base_dir ){
-  // expose global objects
-  require( './lib/global' )( base_dir );
-
-  // expose utils to global
-  require( './lib/utils' );
-
-  // dispatch actions
-  if( CONF.actions === undefined ) return console.log(
-    UTILS.$alert( 'error' ) + '   actions not specified in config'
-  );
-
-  if( UTILS.is( CONF.actions ) !== 'array' ){
-    throw new Error( '[vodka][configs] actions must be an array' );
-  }
-
-  var Flow        = require( 'node.flow' );
-  var outter_flow = new Flow();
-  var Dispatcher  = require( './lib/dispatcher' );
-  var Client      = require( './lib/client' );
-
-  CONF.actions.forEach( function ( file_name ){
-    outter_flow.series( function ( outter_next ){
-      var Action          = require( ACTION_DIR + '/' + file_name );
-      var client          = new Client( file_name );
-      var action_instance = new Action( client );
-      var flow            = new Flow();
-
-      client.actions.forEach( function ( action ){
-        flow.series( function ( next ){
-          if( action.method === 'run' ){
-            return action.action.apply( action_instance, [].slice.call( arguments ));
-          }
-
-          new Dispatcher( action.method, action.uri, action_instance, action.action, action.handler_name, next );
-        });
-      });
-
-      flow.end( function (){
-        outter_next();
-      });
-    });
-  });
-
-  outter_flow.end( function (){
-    console.log( UTILS.$good( 'All test passed :)' ));
-    process.exit( 0 );
-  });
-};
-
-vodka.version = JSON.parse( fs.readFileSync( __dirname + '/package.json', 'utf8' )).version;
+vodka.configs   = require( './lib/configs' );
+vodka.utils     = require( './lib/utils' );
+vodka.fixture   = require( './lib/fixture' );
+vodka.validator = require( './lib/validator' );
+vodka.version   = JSON.parse( fs.readFileSync( __dirname + '/package.json', 'utf8' )).version;
 
 /**
  * Exports module.
---
+++ lib/cli/generator/templates/README.md
@@ -0,0 +1,3 @@
+# Functional Test with VODKA
+
+Check detail on Github : [VODKA](https://github.com/dreamerslab/vodka)
